74f6b7b8ce811bc09b9d5ac81e9b7e7933f7392d
dongyin <windycandy@126.com>
2016-12-17 14:55:07 +0800

    test

--- server.js
+++ server.js
@@ -15,9 +15,32 @@ var app = require('./app');
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
-app.listen(PORT, function () {
-  console.log('Node app is running, port:', PORT);
 
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+    return currentdate;
+}
+
+
+app.listen(PORT, function () {
+  console.log('Windycandy Node app is running, port:', PORT);
+
+  console.log('start ', getNowFormatDate());
+  
   // 注册全局未捕获异常处理器
   process.on('uncaughtException', function(err) {
     console.error("Caught exception:", err.stack);
